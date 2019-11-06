21adac9de668c4418b790531c14d17419d71a2d3
majamii <majami@gmx.net>
2019-11-06 16:22:55 +0100

    erstes Component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  toDoArray: Array<string> = [];
+
   title = 'myApp';
+  item: any;
+
+  addToDo(item: string) {
+    this.toDoArray.push(item);
+    console.log(this.toDoArray);
+
+  }
 }
